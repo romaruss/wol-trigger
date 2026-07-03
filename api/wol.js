5fac9ec14685743de4c076055a482a96b8b234e9
romaruss <piercrup@gmail.com>
2026-07-03 21:52:17 +0200

    Create wol.js

---
+++ api/wol.js
@@ -0,0 +1,20 @@
+import dgram from 'dgram';
+
+export default function handler(req, res) {
+  const { token } = req.query;
+  if (token !== process.env.SECRET_TOKEN) {
+    return res.status(403).send('Non autorizzato');
+  }
+
+  const mac = process.env.SERVER_MAC.replace(/:/g, '');
+  const magic = Buffer.alloc(6, 0xFF);
+  const macBuf = Buffer.from(mac, 'hex');
+  const packet = Buffer.concat([magic, ...Array(16).fill(macBuf)]);
+
+  const socket = dgram.createSocket('udp4');
+  socket.send(packet, 9, process.env.HOME_HOST, (err) => {
+    socket.close();
+    if (err) return res.status(500).send('Errore invio');
+    res.status(200).send('WOL inviato');
+  });
+}
